a8df24227c937a21e6c6869e64dc04d5f3bac9ec
codeamac07 <codeamac07@gmail.com>
2017-07-18 12:08:46 -0500

    July 18th 17 II

---
+++ Programacion/Ruby_2/Seccion-5/nested_array_2.rb
@@ -0,0 +1,21 @@
+=begin
+| Nombre           | Edad | Genero    | Grupo    | Calificaciones |
++------------------+------+-----------+----------+----------------+
+|Rodrigo García    | 13   | Masculino | Primero  |[9, 9, 7, 6, 8] |                 
+|Fernanda Gonzalez | 12   | Femenino  | Tercero  |[6, 9, 8, 6, 8] |
+|Luis Perez        | 13   | Masculino | Primero  |[8, 7, 7, 9, 8] |
+|Ana Espinosa      | 14   | Femenino  | Segundo  |[9, 9, 6, 8, 8] |
+|Pablo Moran       | 11   | Masculino | Segundo  |[7, 8, 9, 9, 8] |
+=end
+
+calif = [{"Nombre"=>"Rodrigo García", "Edad"=>13, "Genero"=>"Masculino", "Grupo"=>"Primero", "Calificaciones"=>[9, 9, 7, 6, 8]},
+    {"Nombre"=>"Fernanda Gonzalez", "Edad"=>12, "Genero"=>"Femenino", "Grupo"=>"Tercero", "Calificaciones"=>[6, 9, 8, 6, 8]},
+    {"Nombre"=>"Luis Perez", "Edad"=>13, "Genero"=>"Masculino", "Grupo"=>"Primero", "Calificaciones"=>[8, 7, 7, 9, 8]},
+    {"Nombre"=>"Ana Espinosa", "Edad"=>14, "Genero"=>"Femenino", "Grupo"=>"Segundo", "Calificaciones"=>[9, 9, 6, 8, 8]},
+    {"Nombre"=>"Pablo Moran", "Edad"=>11, "Genero"=>"Masculino", "Grupo"=>"Segundo", "Calificaciones"=>[7, 8, 9, 9, 8]}]
+
+p calif[0]
+p calif[1].values_at("Nombre", "Calificaciones")
+p calif[2].key("Luis Perez")
+p calif[3].values
+p calif[4].keys
